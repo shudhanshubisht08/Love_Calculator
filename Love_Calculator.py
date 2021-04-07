7375cafa08151e322ce7af3806257d392a71ec4b
Shudhanshu Bisht <sdbisht08@gmail.com>
2021-04-07 09:10:47 +0530

    done

---
+++ Love_Calculator.py
@@ -0,0 +1,26 @@
+print("Welcome to the Love Calculator")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+combine = name1 + name2
+lower_case_string = combine.lower()
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
+true = t + r + u + e
+
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
+love = l + o + v + e
+
+love_score = int(str(true) + str(love))
+
+if (love_score < 10) or (love_score > 90):
+    print(f"Your love score is {love_score}, you go together like coke and mantos 💥")
+elif (love_score <= 40) or (love_score >= 50):
+    print(f"Your score is {love_score}, you are alright together ❤ ")
+else:
+    print(f"Your Score is {love_score} ")
